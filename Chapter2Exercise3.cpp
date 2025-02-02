76155ad361e879314e4d6e850bc3f9574df9373b
Patrick Liao <patliao556@gmail.com>
2025-02-02 12:18:45 -0500

    added user input functionality, part 2

--- Chapter2Exercise3.cpp
+++ Chapter2Exercise3.cpp
@@ -1,7 +1,7 @@
-/*Program File Name: Chapter2Exercise3
+/*Program File Name: Chapter2Exercise3Part2
 Programmer: Patrick Liao
-Date: 1.26.2025
-Requirements: Write a program that will compute the total sales tax on a $95 purchase. Assume the sales tax is 4 percent, and the county sales tax is 2 percent.*/
+Date: 2.22.2025
+Requirements: Write a program that will compute the total sales tax given a user inputted purchase amount and sales and county tax rate.*/
 
 #include <iostream>
 #include <iomanip>
@@ -9,9 +9,19 @@ using namespace std;
 
 int main()
 {   
-    double purchase = 95.00;                                                                                                                                                //total purchase amount
-    double staxrate = 4;                                                                                                                                                    //sales tax rate
-    double ctaxrate = 2;                                                                                                                                                    //county tax rate
+    double purchase;                                                                                                                                                    //total purchase amount
+    double staxrate;                                                                                                                                                    //sales tax rate
+    double ctaxrate;                                                                                                                                                    //county tax rate
+
+    cout << "How much did you purchase?: ";
+    cin >> purchase;                                                                                                                                                    
+
+    cout << "What is the sales tax rate?: ";
+    cin >> staxrate;
+
+    cout << "What is the county tax rate?: ";
+    cin >> ctaxrate; ;                                                                                                                                                   //prompt user for purchase,sales tax, and county tax
+
     float salestax = (purchase * (staxrate/100)) + (purchase * (ctaxrate/100));                                                                                             //calculate total tax amount
         
     cout << "With a purchase of $" << purchase << ", a sales tax of " << staxrate << "%, and a county sales tax of " << ctaxrate << "%, the total sales tax is $";          //display message
